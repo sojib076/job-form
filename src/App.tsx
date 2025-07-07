854995547abeafb82001d81e6fd418600bd7ba3f
sojib076 <sojibds332@gmail.com>
2025-07-08 00:19:23 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <h1 className='text-3xl font-bold underline text-center mt-10'>
+          Vite + React + TypeScript
+        </h1>
+    </div>
+  )
+}
+
+export default App
